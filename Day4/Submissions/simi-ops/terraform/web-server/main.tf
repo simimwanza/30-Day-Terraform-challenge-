09d2af5ab850be6449323c300861c68d83334484
simimwanza <simimwanza@gmail.com>
2025-05-30 12:58:03 +0000

    Added the use of variables and data source to the code

--- Day4/Submissions/simi-ops/terraform/web-server/main.tf
+++ Day4/Submissions/simi-ops/terraform/web-server/main.tf
@@ -1,7 +1,23 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-west-2"
+  region = var.aws_region
 }
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_security_group" "ec2_sg" {
   name        = "web-server-sg"
   description = "Allow web traffic and SSH access"
@@ -43,8 +59,8 @@ resource "aws_security_group" "ec2_sg" {
 }
 
 resource "aws_instance" "web_server" {
-  ami           = "ami-04999cd8f2624f834" 
-  instance_type = "t2.micro"             
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type            
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   user_data = <<-EOF
@@ -53,7 +69,7 @@ resource "aws_instance" "web_server" {
               yum install -y httpd
               systemctl start httpd
               systemctl enable httpd
-              echo "<h1>Hello World from Terraform 30 Day Challenge</h1>" > /var/www/html/index.html
+              echo "<h1>Hello World from Terraform 30 Day Challenge: Day 4</h1>" > /var/www/html/index.html
               EOF
 
   tags = {
